8f6ce02d5e39203693e6ff9e7b689400cf6f6cb0
Abdul Wasay <wasayhayee@gmail.com>
2020-03-10 19:49:23 +0500

    Port Changed

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const { mongodbUrl } = require('./config/database');
-
+const http = require('http');
+const server = http.Server(app);
+const PORT = process.env.PORT || 3000;
 mongoose.connect(mongodbUrl, { useNewUrlParser: true }).then((db) => {
     console.log("Mongodb is connected");
 }).catch(error => console.log(error));
+server.listen(PORT, () => {
+    console.log("Port is working PORT} ");
+})
+// app.listen(3000, () => {
+//     console.log("Listening at port 3000");
+// });
