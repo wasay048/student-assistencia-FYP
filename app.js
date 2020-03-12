151039ca67018aead6cc40df2578ccb6950dffc5
Abdul Wasay <wasayhayee@gmail.com>
2020-03-12 13:56:33 +0500

    route setup for testing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const { mongodbUrl } = require('./config/database');
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(mongodbUrl, { useNewUrlParser: true }).then((db) => {
     console.log("Mongodb is connected");
 }).catch(error => console.log(error));
+app.get('/contact', (req, res) => {
+    res.send("It is working fine.");
+})
 server.listen(PORT, () => {
     console.log("Port is working PORT} ");
 })
